e0a1b1302876d75fe3da74d397e9f957684f2a6f
Igor <igorigorenko00@gmail.com>
2016-11-21 00:04:41 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+
+#include <opencv2/core/core.hpp> 
+#include <opencv2/opencv.hpp> 
+#include <opencv2/highgui/highgui.hpp> 
+#include <iostream> 
+
+IplImage* image = 0;
+IplImage* dst = 0;
+int vals = 0;
+
+
+void findRectangles(IplImage* _image) 
+{ 
+assert(_image!=0); 
+ 
+IplImage* bin = cvCreateImage( cvGetSize(_image), IPL_DEPTH_8U, 1); 
+// конвертируем в градации серого 
+cvConvertImage(_image, bin, CV_BGR2GRAY); 
+// находим границы 
+cvCanny(bin, bin, 50, 200); 
+cvNamedWindow( "bin", 1 ); 
+cvShowImage("bin", bin); 
+// хранилище памяти для контуров 
+CvMemStorage* storage = cvCreateMemStorage(0); 
+CvSeq* contours=0; 
+ 
+// находим контуры 
+int contoursCont = cvFindContours( bin, storage,&contours,sizeof(CvContour),CV_RETR_LIST,CV_CHAIN_APPROX_SIMPLE,cvPoint(0,0)); 
+ 
+assert(contours!=0); 
+
+// обходим все контуры 
+for( CvSeq* current = contours; current != NULL; current = current->h_next ){ 
+// вычисляем площадь и периметр контура 
+double area = fabs(cvContourArea(current)); 
+double perim = cvContourPerimeter(current); 
+ 
+ 
+if ( area / (perim*perim/16) > 0.7 && area / (perim*perim/16) < 1.3 ){ // в 10% интервале 
+	vals++;
+cvDrawContours(_image, current, cvScalar(0, 0, 255), cvScalar(0, 255, 0), -1, 1, 8); 
+} 
+} 
+ 
+ 
+cvReleaseMemStorage(&storage); 
+cvReleaseImage(&bin); 
+} 
+
+
+int main(int argc, char* argv[])
+{
+        char* filename = argc == 2 ? argv[1] : "Sqrs2.jpg";
+        image = cvLoadImage(filename,1); 
+        dst = cvCloneImage(image);
+      //  printf("[i] image: %s\n", filename);
+        assert( image != 0 );
+        // окно для отображения картинки
+       // cvNamedWindow("original",CV_WINDOW_AUTOSIZE);
+     // cvNamedWindow("smooth",CV_WINDOW_AUTOSIZE);
+        // сглаживаем исходную картинку
+        //cvSmooth(image, dst, CV_GAUSSIAN, 3, 3);
+		//cvSmooth(image, dst, CV_GAUSSIAN, 3, 3);
+
+		//cvSmooth(image, dst, CV_BILATERAL, 3, 3);
+		//cvSmooth(image, dst, CV_GAUSSIAN, 3, 3);
+		cvSmooth(image, dst, CV_MEDIAN ,3, 3 );
+
+			cvNamedWindow( "final", 1 ); 
+			cvShowImage( "final", dst); 
+			//dst = cvCloneImage(image); 
+			// находим круги на изображении 
+			findRectangles(dst); 
+			cvNamedWindow( "rectangles", 1 ); 
+			cvShowImage( "rectangles", dst); 
+			// ждём нажатия клавиши 
+			cvWaitKey(0);  
+			// освобождаем ресурсы 
+			cvReleaseImage(&dst); 
+			cvReleaseImage(&dst); 
+			// удаляем окна 
+			cvDestroyAllWindows(); 
+			return 0; 
+}
